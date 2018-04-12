af53952180fee0067e1482eea6f6f973f44968e0
AllisonCook27 <allison.cook@ocsbstudent.ca>
2018-04-12 13:45:50 -0400

    Initial commit

---
+++ AlphabetNestedLoopsAllison/AlphabetForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace AlphabetNestedLoopsAllison
+{
+    partial class frmAlphabet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnstart = new System.Windows.Forms.Button();
+            this.lstLetters = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnstart
+            // 
+            this.btnstart.Font = new System.Drawing.Font("Monospac821 BT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnstart.Location = new System.Drawing.Point(97, 23);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(78, 25);
+            this.btnstart.TabIndex = 0;
+            this.btnstart.Text = "Start";
+            this.btnstart.UseVisualStyleBackColor = true;
+            this.btnstart.Click += new System.EventHandler(this.btnstart_Click);
+            // 
+            // lstLetters
+            // 
+            this.lstLetters.Font = new System.Drawing.Font("Monospac821 BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstLetters.FormattingEnabled = true;
+            this.lstLetters.ItemHeight = 19;
+            this.lstLetters.Location = new System.Drawing.Point(65, 63);
+            this.lstLetters.Name = "lstLetters";
+            this.lstLetters.Size = new System.Drawing.Size(141, 194);
+            this.lstLetters.TabIndex = 1;
+            // 
+            // frmAlphabet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lstLetters);
+            this.Controls.Add(this.btnstart);
+            this.Name = "frmAlphabet";
+            this.Text = "Alphabet Nested Loops by Allison C";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnstart;
+        private System.Windows.Forms.ListBox lstLetters;
+    }
+}
+
---
+++ AlphabetNestedLoopsAllison/AlphabetForm.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Created by: Allison Cook
+ * Created on: 12 April, 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #28 - Alphabet Nested Loops
+ * This program...
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlphabetNestedLoopsAllison
+{
+    public partial class frmAlphabet : Form
+    {
+        public frmAlphabet()
+        {
+            InitializeComponent();
+        }
+
+        private void btnstart_Click(object sender, EventArgs e)
+        {
+            //variables
+            int counter1, counter2;
+            string letter1, letter2;
+
+            for (counter1 = 65; counter1 <= 90; counter1 ++)
+            {
+                //convert to letter
+                letter1 = Char.ConvertFromUtf32(counter1);
+
+                for(counter2 = 97; counter2 <= 122; counter2++)
+                {
+                    //convert to letter                                                                                                                                           
+                    letter2 = Char.ConvertFromUtf32(counter2);
+
+                    //display the letters
+                    lstLetters.Items.Add(letter1 + " -> " + letter2);
+                }
+            }
+        }
+    }
+}
